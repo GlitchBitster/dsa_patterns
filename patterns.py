202e2dd8ea06c221bcd727c029ee88e6219692cf
GlitchBitster <49645988+GlitchBitster@users.noreply.github.com>
2025-10-24 14:57:58 +0530

    Initial Commit

---
+++ patterns.py
@@ -0,0 +1,35 @@
+def PatternA(num):
+    for i in range(num):
+        for j in range(num):
+            print("*", end=" ")
+        print("")
+
+def PatternB(num):
+    for i in range(num):
+        for j in range(num):
+            print(i + 1, end=" ")
+        print("")
+
+def PatternC(num):
+    for i in range(num):
+        for j in range(i+1):
+            print("*", end=" ")
+        print("")
+        
+def PatternD(num):
+    for i in range(num):
+        for j in range(i+1):
+            print(j+ 1, end=" ")
+        print("")
+
+print("----Pattern A----")
+PatternA(5)
+
+print("----Pattern B----")
+PatternB(5)
+
+print("----Pattern C----")
+PatternC(5)
+
+print("----Pattern D----")
+PatternD(5)
